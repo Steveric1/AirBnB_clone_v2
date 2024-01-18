b45f6831bc6722859546f29dd6ac260adf1b7858
Eric <stephen.erich243@gmail.com>
2024-01-18 01:24:31 +0100

    updated

--- models/user.py
+++ models/user.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import create_engine
+from sqlalchemy import Column, String
 from sqlalchemy.ext.declarative import declarative_base
 
-# engine = create_engine('mysql://root:password@locahost/hbnb_dev_db', echo=True)
-# Base = declarative_base()
+Base = declarative_base()
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'user'
-    
-    # id = Column(Integer, primary_key=True)
+    __tablename__ = 'users'  # Corrected table name
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
 
-# Base.metadata.create_all(engine)
