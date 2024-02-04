72e95048a5518e3265151de1c17aabae29fb3786
Eric <stephen.erich243@gmail.com>
2024-02-04 23:23:58 +0100

    nginx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,32 +1,14 @@
 #!/usr/bin/env bash
 #Bash script that sets up your web servers for the deployment of web_static
 
-#update and install nginx
-sudo apt-get -y update
-sudo apt-get -y install nginx
-
-function configure_nginx {
-
-    sudo mkdir -p "/data/web_static/releases/test/"
-    sudo mkdir -p "/data/web_static/shared/"
-    # Create a fake HTML file /data/web_static/releases/test/index.html
-    echo "ALX School" | sudo tee "/data/web_static/releases/test/index.html" > /dev/null
-
-    # Set ownership recursively to ubuntu user and group
-    sudo chown -R ubuntu:ubuntu /data
-
-    #create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder
-    if [ ! -L /data/web_static/current ]; then
-        sudo ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
-    fi
-
-    #Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
-    if [ -f /etc/nginx/sites-available/default ]; then
-        sudo sed -i "38i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" "/etc/nginx/sites-available/default"
-        #restart nginx
-        sudo service nginx restart
-    fi
-}
-
-#call the function
-configure_nginx
+server="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
+file="/etc/nginx/sites-available/default"
+sudo apt-get update -y
+sudo apt-get install nginx -y
+sudo mkdir -p "/data/web_static/releases/test/"
+sudo mkdir "/data/web_static/shared/"
+echo "Holberton" > "/data/web_static/releases/test/index.html"
+rm -f "/data/web_static/current"; ln -s "/data/web_static/releases/test/" "/data/web_static/current"
+sudo chown -hR ubuntu:ubuntu "/data/"
+sudo sed -i "29i\ $server" "$file"
+sudo service nginx restart
