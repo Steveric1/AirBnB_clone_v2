9794e156ac6683c74c14169a87e1d21b302634a3
Eric <stephen.erich243@gmail.com>
2024-03-13 08:33:28 +0100

    updated

--- replace.py
+++ replace.py
@@ -2,13 +2,26 @@
 
 # def escape_double_quotes(input_string):
 """This module defines a function to escape double quotes in a string"""
+import shlex
 
+s = "quoted_string_with_underscores"
+tokens = shlex.split(s)
+quote = shlex.quote(s)
 
-def escape_double_quotes(input_string):
-    esc_string = ""
-    for char in input_string:
-        if char == '"':
-            esc_string += r'\"'
-        else:
-            esc_string += char
-    return esc_string
+for token in shlex.split(s):
+    if '_' in token:
+        key = token.split('_').replace('_', ' /')
+        
+print(tokens)
+# print(quote)
+
+# def escape_double_quotes(input_string):
+#     new_dict = {}
+#     for arg in input_string:
+#         if '=' in arg:
+#             k = arg.split('=', 1)
+#             value = k[1]
+#             key = k[0]
+#             
+#             if value[0] == value[-1] == '"':
+                
